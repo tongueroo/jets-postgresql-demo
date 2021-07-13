787fe91c49c18b3900c0add9b40042dc0aff7604
Tung Nguyen <tongueroo@gmail.com>
2021-07-13 20:26:34 +0000

    add info controller

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,6 +1,7 @@
 class InfoController < ApplicationController
   def index
     render json: {
+      db_adapter: ActiveRecord::Base::connection.class.to_s,
       version: ActiveRecord::Base.connection.execute("SELECT version()")
     }
   end
